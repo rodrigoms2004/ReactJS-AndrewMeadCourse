bb2136d61b72f50e659150d6dd039c2fb8765f77
Rodrigo Moraes Silveira <rodrigoms2004@gmail.com>
2020-07-15 02:26:31 -0300

    setting up Babel

--- projects/indecision-app/public/scripts/app.js
+++ projects/indecision-app/public/scripts/app.js
@@ -1,15 +1,14 @@
+'use strict';
+
 console.log('App.js is running');
 
 // JSX - Javascript XML
-// var template = <p>This is JSX from app.js!</p>;
-
-// https://reactjs.org/docs/react-api.html#createelement
 var template = React.createElement(
-  'p', 
-  { id: 'someid'}, 
-  'This is JSX from app.js!');
+  'h1',
+  null,
+  'Indecision App'
+);
 
-
-var appRoot = document.getElementById('app')
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);

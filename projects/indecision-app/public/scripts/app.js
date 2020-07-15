2ae0ee6270ed17e351d7b75eee84b5331ad78b45
Rodrigo Moraes Silveira <rodrigoms2004@gmail.com>
2020-07-15 03:26:39 -0300

    JSX

--- projects/indecision-app/public/scripts/app.js
+++ projects/indecision-app/public/scripts/app.js
@@ -1,12 +1,84 @@
 'use strict';
 
 console.log('App.js is running');
+// $ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch 
+
 
 // JSX - Javascript XML
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var user = {
+  name: 'Rodrigo',
+  age: 38,
+  location: 'São Paulo'
+};
+
+var getLocation = function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+  // return location ? <p>Location: {location}</p> : ''
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- projects/indecision-app/src/app.js
+++ projects/indecision-app/src/app.js
@@ -1,7 +1,47 @@
 console.log('App.js is running');
+// $ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch 
+
 
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+}
+
+var template = (
+  <div>
+    <h1>{app.title}</h1> 
+    {(app.subtitle && <p>{app.subtitle}</p>)}
+    <p>{(app.options && app.options.length > 0 ? 'Here are your options' : 'No options')}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+const user = {
+  name : 'Rodrigo',
+  age : 38,
+  location : 'São Paulo'
+};
+
+const getLocation = location => {
+  if (location) {
+    return <p>Location: {location}</p>  
+  }
+  // return location ? <p>Location: {location}</p> : ''
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
 
 var appRoot = document.getElementById('app')
 

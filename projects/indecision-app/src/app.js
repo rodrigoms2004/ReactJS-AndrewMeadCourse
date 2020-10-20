e244d251a066375ae1d6c78c6dd074d22305af78
Rodrigo Moraes Silveira <rodrigoms2004@gmail.com>
2020-10-20 03:47:20 -0300

    ES6 import/export

--- projects/indecision-app/src/app.js
+++ projects/indecision-app/src/app.js
@@ -1,1 +1,15 @@
-console.log('app.js is running')
+// yarn run serve
+// yarn run build
+
+
+// import './utils'
+// import { square, add } from './utils'
+
+// console.log('app.js is running')
+// console.log(square(4))
+// console.log(add(100, 23))
+
+import {isAdult, canDrink} from './person'
+
+console.log(isAdult(16))
+console.log(canDrink(25))
